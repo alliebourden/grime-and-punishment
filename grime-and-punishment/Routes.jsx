dfa40f9ec5090d3ce3e1400fdd6d01851788bbf8
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-04-13 19:36:29 -0230

    install react router

---
+++ grime-and-punishment/Routes.jsx
@@ -0,0 +1,73 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/",
+//       element: <Layout />,
+//       children: [
+//         { path: "/dashboard", element: <Dashboard /> },
+//         {
+//           path: "campaign-summary",
+//           element: (
+//             <div className="campaign-summary">
+//               <CampaignSummary />
+//             </div>
+//           ),
+//         },
+//         {
+//           path: "/quest-tracker",
+//           element: (
+//             <div className="quest-tracker">
+//               <QuestTracker />
+//             </div>
+//           ),
+//         },
+//         {
+//           path: "/loot-manager",
+//           element: (
+//             <div className="loot-manager">
+//               <LootManager />
+//             </div>
+//           ),
+//         },
+//         {
+//           path: "/session-editor",
+//           element: (
+//             <div>
+//               <SessionEditor />
+//             </div>
+//           ),
+//         },
+//         {
+//           path: "/session-list",
+//           element: (
+//             <div className="sessions">
+//               <SessionList />
+//             </div>
+//           ),
+//         },
+//         {
+//           path: "/quest-tracker-form",
+//           element: (
+//             <div>
+//               <QuestTrackerForm />
+//             </div>
+//           ),
+//         },
+
+//         {
+//           path: "/about",
+//           element: (
+//             <div className="about">
+//               <About />
+//             </div>
+//           ),
+//         },
+//       ],
+//     },
+//   ],
+//   { basename: import.meta.env.DEV ? "/" : "/fableforge/" }
+// );
+
+// export default router;
