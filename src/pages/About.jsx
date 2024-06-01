613750496a0066175aaca5d30f3dcb93d17130e0
alliebourden <127435154+alliebourden@users.noreply.github.com>
2024-06-01 21:16:50 -0230

    build - added hero to home page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import HomeHero from '../components/HomeHero';
 
 const About = () => {
   return (
     <>
       <NavBar />
+      <HomeHero />
     </>
   );
 }
